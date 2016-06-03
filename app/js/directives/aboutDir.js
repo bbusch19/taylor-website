8b8a1e479e3e59843672160faff526f7663b2d12
Breiden Busch <breiden.busch@gmail.com>
2016-06-03 17:09:59 -0600

    About Section Styling

---
+++ app/js/directives/aboutDir.js
@@ -0,0 +1,11 @@
+angular.module('taylorApp').directive('aboutDir', function() {
+
+    function link(scope, elem, attrs) {
+
+    }
+
+    return {
+        restrict: 'E',
+        templateUrl: '../views/aboutDir.html',
+    }
+})
